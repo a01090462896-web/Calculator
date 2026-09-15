2e407b46dc1d082ece87b2ae82739dbe912ffcd0
눈꽃바램 <a01090462896@gmail.com>
2026-09-15 20:53:17 +0900

    깔끔하게 정리

--- src/Main.java
+++ src/Main.java
@@ -5,49 +5,48 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.println("첫번쨰 양의 정수를 입력하세요: ");
+            int intBox = scanner.nextInt();
+                System.out.println("intBox = " + intBox);
 
-        System.out.println("첫번쨰 양의 정수를 입력하세요: ");
-        int intBox = scanner.nextInt();
-        System.out.println("intBox = " + intBox);
+                System.out.println("두번째 양의 정수를 입력하세요: ");
+                int intBox2 = scanner.nextInt();
+                System.out.println("intBox2 = " + intBox2);
 
-        System.out.println("두번째 양의 정수를 입력하세요: ");
-        int intBox2 = scanner.nextInt();
-        System.out.println("intBox2 = " + intBox2);
+                System.out.println("사칙연산 기호를 입력하세요: ");
+                String operator = scanner.next();
+                System.out.println("intBox3 = " + operator);
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        String intBox3 = scanner.next();
-        System.out.println("intBox3 = " + intBox3);
-
-        int result = 0;
-
-        switch (intBox3) {
-
-            case "+":
-                result = intBox + intBox2;
-                break;
-            case "-":
-                result = intBox - intBox2;
-                break;
-            case "*":
-                result = intBox * intBox2;
-                break;
-            case "/":
-                if (intBox2 == 0) {
+                if (operator.equals("/") && intBox2 == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에는 0이 입력될 수없습니다.");
-                } else {
-                    result = intBox / intBox2;
-                }
-                break; }
-            if (!(intBox3.equals("/") && intBox2 == 0)) {
-                System.out.println("결과: " + result);
+                continue;
             }
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exitCheck = scanner.next();
 
-            if (exitCheck.equals("exit")) {
-                System.out.println("계산기를 종료합니다.");
-                break;
+                int result = 0;
+
+                switch (operator) {
+                    case "+":
+                        result = intBox + intBox2;
+                        break;
+                    case "-":
+                        result = intBox - intBox2;
+                        break;
+                    case "*":
+                        result = intBox * intBox2;
+                        break;
+                    case "/":
+                        result = intBox / intBox2;
+                        break;
+                }
+
+                    System.out.println("결과: " + result);
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    String exitCheck = scanner.next();
+
+                    if (exitCheck.equals("exit")) {
+                        System.out.println("계산기를 종료합니다.");
+                    break;
+                }
             }
         }
     }
-}
