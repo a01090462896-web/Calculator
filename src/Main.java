a59eedc73f085c2af510e4d7e4528ae5b242c57e
눈꽃바램 <a01090462896@gmail.com>
2026-09-15 17:23:35 +0900

    사칙연산 기호를 적합한 타입으로 입력받기

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,14 @@ public class Main {
 
         System.out.println("첫번쨰 양의 정수를 입력하세요: ");
         int intBox = scanner.nextInt();
+        System.out.println("intBox = " + intBox);
+
         System.out.println("두번째 양의 정수를 입력하세요: ");
         int intBox2 = scanner.nextInt();
+        System.out.println("intBox2 = " + intBox2);
 
         System.out.println("사칙연산 기호를 입력하세요: ");
-        String operator = scanner.next();
-
+        String intBox3 = scanner.next();
+        System.out.println("intBox3 = " + intBox3);
     }
 }
