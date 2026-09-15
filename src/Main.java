4f37bc90351090b6c99869fe54882bf9b38bbc30
눈꽃바램 <a01090462896@gmail.com>
2026-09-15 21:07:34 +0900

    주석 사용

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            // 첫 번째 정수 입력 및 음수 검사
             System.out.println("첫번쨰 양의 정수를 입력하세요: ");
             int intBox = scanner.nextInt();
             System.out.println("intBox = " + intBox);
@@ -14,6 +15,7 @@ public class Main {
                 continue;
             }
 
+            // 두 번째 정수 입력 및 음수 검사
             System.out.println("두번째 양의 정수를 입력하세요: ");
             int intBox2 = scanner.nextInt();
             System.out.println("intBox2 = " + intBox2);
@@ -27,6 +29,7 @@ public class Main {
             String operator = scanner.next();
             System.out.println("intBox3 = " + operator);
 
+            // 나누기 에러 검사
             if (operator.equals("/") && intBox2 == 0) {
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에는 0이 입력될 수없습니다.");
                 continue;
@@ -49,7 +52,10 @@ public class Main {
                     break;
             }
 
+            // 결과 출력
             System.out.println("결과: " + result);
+
+            // 종료 여부 확인
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exitCheck = scanner.next();
 
