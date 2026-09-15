369415905f47d727ee054e409b011294e56703cf
눈꽃바램 <a01090462896@gmail.com>
2026-09-15 16:43:11 +0900

    양의 정수 2개 입력받는 기능 구현

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("첫번쨰 양의 정수를 입력하세요: ");
+        int intBox = scanner.nextInt();
+        System.out.println("두번째 양의 정수를 입력하세요: ");
+        int intBox2 = scanner.nextInt();
+
+    }
+}
