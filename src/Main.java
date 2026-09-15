b135727a1ed5fe61f697e907ad4ae685ca82e437
눈꽃바램 <a01090462896@gmail.com>
2026-09-15 20:59:06 +0900

    첫 번째 숫자와 두 번째 숫자가 양수가 아닌 숫자가 입력 되었을시 처음으로 돌아감

--- src/Main.java
+++ src/Main.java
@@ -7,46 +7,56 @@ public class Main {
         while (true) {
             System.out.println("첫번쨰 양의 정수를 입력하세요: ");
             int intBox = scanner.nextInt();
-                System.out.println("intBox = " + intBox);
+            System.out.println("intBox = " + intBox);
 
-                System.out.println("두번째 양의 정수를 입력하세요: ");
-                int intBox2 = scanner.nextInt();
-                System.out.println("intBox2 = " + intBox2);
-
-                System.out.println("사칙연산 기호를 입력하세요: ");
-                String operator = scanner.next();
-                System.out.println("intBox3 = " + operator);
-
-                if (operator.equals("/") && intBox2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에는 0이 입력될 수없습니다.");
+            if (intBox < 0) {
+                System.out.println("양의 정수만 입력 가능합니다");
                 continue;
             }
 
-                int result = 0;
+            System.out.println("두번째 양의 정수를 입력하세요: ");
+            int intBox2 = scanner.nextInt();
+            System.out.println("intBox2 = " + intBox2);
 
-                switch (operator) {
-                    case "+":
-                        result = intBox + intBox2;
-                        break;
-                    case "-":
-                        result = intBox - intBox2;
-                        break;
-                    case "*":
-                        result = intBox * intBox2;
-                        break;
-                    case "/":
-                        result = intBox / intBox2;
-                        break;
-                }
+            if (intBox2 < 0) {
+                System.out.println("양의 정수만 입력 가능합니다");
+                continue;
+            }
 
-                    System.out.println("결과: " + result);
-                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                    String exitCheck = scanner.next();
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            String operator = scanner.next();
+            System.out.println("intBox3 = " + operator);
 
-                    if (exitCheck.equals("exit")) {
-                        System.out.println("계산기를 종료합니다.");
+            if (operator.equals("/") && intBox2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에는 0이 입력될 수없습니다.");
+                continue;
+            }
+
+            int result = 0;
+
+            switch (operator) {
+                case "+":
+                    result = intBox + intBox2;
                     break;
-                }
+                case "-":
+                    result = intBox - intBox2;
+                    break;
+                case "*":
+                    result = intBox * intBox2;
+                    break;
+                case "/":
+                    result = intBox / intBox2;
+                    break;
+            }
+
+            System.out.println("결과: " + result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exitCheck = scanner.next();
+
+            if (exitCheck.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
             }
         }
     }
+}
