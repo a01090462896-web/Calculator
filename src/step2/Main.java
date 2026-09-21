b7622b7edc69949530e5f0984e964969c0073a1b
눈꽃바램 <a01090462896@gmail.com>
2026-09-21 15:18:20 +0900

    이름변경

--- src/step2/Main.java
+++ src/step2/Main.java
@@ -2,7 +2,7 @@ package step2;
 
 import java.util.Scanner;
 
-public class App {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
