07f2d866362a6df26c7d1ed0ff05f677bd9fdccc
눈꽃바램 <a01090462896@gmail.com>
2026-09-21 12:27:27 +0900

    ArithmeticCalculator클래스 활용준비로 Calculator내용 복사후 붙혀넣음

---
+++ src/step2/ArithmeticCalculator.java
@@ -0,0 +1,45 @@
+package step2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArithmeticCalculator {
+
+    private List<Integer> resultList = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+        resultList.add(result);
+
+        return result;
+    }
+
+    public List<Integer> getResultList() {
+        return resultList;
+    }
+
+    public void setResultList(List<Integer> resultList) {
+        this.resultList = resultList;
+    }
+
+    // 저장된 기록 삭제하는 메서드
+    public void removeResult() {
+        if (!resultList.isEmpty()) {
+            resultList.remove(0);
+        }
+    }
+}
