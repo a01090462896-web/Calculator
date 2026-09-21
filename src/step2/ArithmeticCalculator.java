d8a2dac8f953cba719905e0028985d9994ae4555
눈꽃바램 <a01090462896@gmail.com>
2026-09-21 19:12:58 +0900

    주석 추가와 입력받은 값보다 큰 결과값 출력

--- src/step2/ArithmeticCalculator.java
+++ src/step2/ArithmeticCalculator.java
@@ -3,7 +3,7 @@ package step2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArithmeticCalculator <T extends Number> {
+public class ArithmeticCalculator<T extends Number> {
     private List<Double> resultList = new ArrayList<>();
 
     public enum OperatorType {
@@ -21,6 +21,7 @@ public class ArithmeticCalculator <T extends Number> {
             return symbol;
         }
     }
+
     public double calculate(T num1, T num2, OperatorType operator) {
         double n1 = num1.doubleValue();
         double n2 = num2.doubleValue();
@@ -43,28 +44,39 @@ public class ArithmeticCalculator <T extends Number> {
                 result = n1 / n2;
                 break;
         }
+        // 연산 결과를 저장
         resultList.add(result);
         return result;
     }
 
+    // 게터
     public List<Double> getResultList() {
         return resultList;
     }
 
+    // 세터
     public void setResultList(List<Double> resultList) {
         this.resultList = resultList;
     }
 
-// 저장된 기록 삭제하는 메서드
+    // 저장된 기록 삭제하는 메서드
     public void removeResult() {
-
         if (!resultList.isEmpty()) {
             resultList.remove(0);
         }
     }
+
+    // 스트림 활용
     public void printResultsGreaterThan(double threshold) {
         resultList.stream()
                 .filter(result -> result > threshold)
                 .forEach(System.out::println);
+    }
+
+    // 특정 값보다 큰 결과값 반환
+    public List<Double> getResultsGreaterThan(double threshold) {
+        return resultList.stream()
+                .filter(result -> result > threshold)
+                .toList();
     }
 }
